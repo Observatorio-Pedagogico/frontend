12ac5032d5cc30c88bb941bfdac135bd81da2b92
Thauan Amorim <thauanssussu@gmail.com>
2022-06-30 21:04:21 -0300

    recuperacao de arquivo via ts

--- src/app/extracao/cadastro-extracao/cadastro-extracao.component.ts
+++ src/app/extracao/cadastro-extracao/cadastro-extracao.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Location } from '@angular/common';
+import { ExtracaoService } from '../services/extracao.service';
+import { Extracao } from '../model/extracao';
 
 @Component({
   selector: 'app-cadastro-extracao',
@@ -8,27 +11,39 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class CadastroExtracaoComponent implements OnInit {
 
-  form: FormGroup;
+    form: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
-    this.form = this.formBuilder.group({
-      titulo: [null],
-      periodoLetivoTipo: [null],
-      periodoLetivo: [null],
-      descricao: [null]
-    });
-   }
-
-  ngOnInit(): void {
-  }
-
-  onFileChange(event: any): void {
-    if (event.target.files && event.target.files[0]) {
-        const file = event.target.files[0];
-
+    constructor(
+        private formBuilder: FormBuilder,
+        private location: Location,
+        private extracaoService: ExtracaoService
+    ) {
+        this.form = this.formBuilder.group({
+        titulo: [null],
+        periodoLetivoTipo: [null],
+        periodoLetivo: [null],
+        descricao: [null]
+        });
     }
 
-  }
+    salvarExtracaoEvent(): void {
+        // if (this.form.status !== 'VALID') return;
 
+        let extracaoForm = this.form.value as Extracao;
 
+        var primeiroArquivo = (document.getElementById("primeiroArquivo") as HTMLInputElement).files?.item(0);
+        if (primeiroArquivo) {
+        extracaoForm.arquivo.conteudo = primeiroArquivo;
+        }
+
+        console.log(extracaoForm.arquivo);
+        this.extracaoService;
+    }
+
+    voltarButtonEvent(): void {
+        this.location.back();
+    }
+
+    ngOnInit(): void {
+    }
 }
--- src/app/extracao/model/extracao.ts
+++ src/app/extracao/model/extracao.ts
@@ -1,8 +1,12 @@
-export interface extracao {
+export interface Extracao {
     id: number,
     titulo: string,
     periodoLetivoTipo: string,
     periodoLetivo: string,
+    arquivo: Arquivo,
     descricao: string
+}
 
+export interface Arquivo {
+    conteudo: File
 }
