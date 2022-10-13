e742d40d9470c1bc0bc688326ab7c3c5a2ef9d08
0jafc0 <jafclive0@gmail.com>
2022-10-13 16:54:10 -0300

    terminado testes envio da extracao, comecado cadastrar e login

--- cypress/e2e/interacao01/interacao01.spec.cy.ts
+++ cypress/e2e/interacao01/interacao01.spec.cy.ts
@@ -1,24 +1,24 @@
 /// <reference types="cypress"/>
 
-
 context("Login na aplicacao", () => {
   it("Usuario logado com sucesso", () => {
     cy.visit("localhost:4200/login")
-    cy.get('input[formControlName="email"]').type('thauan.amorim@academico.ifpb.edu.br')
-    cy.get('input[formControlName="senha"]').type('123456')
+    cy.get('[type="email"]').type('thauan.amorim@academico.ifpb.edu.br')
+    cy.get('[type="password"]').type('123456')
     cy.get('button').click()
+    cy.contains('Tabela de Extrações SUAP')
+
   } )
-} )
+})
 
 context("Nova Extração", () => {
   it("Ir à uma nova extracao", () => {
     cy.get('button[routerLink="nova-extracao"]').click()
   } )
-} )
+})
 
 context("Cadastro Extracao", () => {
-  it("Cadastrando uma nova extracao", () => {
-    cy.get('input[formControlName="titulo"]').type('extracao teste')
+  it("validando campo titulo", () => {
     cy.get('input[formControlName="periodoLetivo"]').type('2022.2')
     cy.get('mat-select[formControlName="periodoLetivoTipo"]').click()
     cy.get('mat-option[value="SEMESTRE"]').click()
@@ -29,9 +29,36 @@ context("Cadastro Extracao", () => {
           'cypress/fixtures/Teste Alunos.xlsx'
         ],
         {force: true})
-    cy.get('textarea[formControlName="descricao"]').type('Extrações referentes as disciplinas de programação 1 e projeto 2, do semestre 2022.2 do curso de Análise e Desenvolvimento de Sistemas, campus monteiro, disciplina ministrada pela professora Renata.')
-    cy.get('button[type="submit"]').click()
-
-
+    cy.get('textarea[formControlName="descricao"]').type('Extrações das disciplinas do semestre 2022.2 do curso de Análise e Desenvolvimento de Sistemas, disciplina ministrada pela professora Renata.')
+    cy.get('button[type="submit"]').should('be.disabled');
   } )
-} )
+
+  it("validando campo periodo letivo", () => {
+    cy.get('input[formControlName="titulo"]').type('testando envio')
+    cy.get('input[formControlName="periodoLetivo"]').clear()
+    cy.get('button[type="submit"]').should('be.disabled');
+  } )
+
+  it("validando campo descricao", () => {
+    cy.get('input[formControlName="periodoLetivo"]').type('2022.2')
+    cy.get('textarea[formControlName="descricao"]').clear()
+    cy.get('button[type="submit"]').should('be.disabled');
+  } )
+
+  it("validando campo files", () => {
+    cy.get('textarea[formControlName="descricao"]').type('Extrações das disciplinas do semestre 2022.2 do curso de Análise e Desenvolvimento de Sistemas, disciplina ministrada pela professora Renata.')
+    cy.get('.p-element.ng-star-inserted > .p-ripple').click()
+    cy.get('button[type="submit"]').should('be.disabled');
+  } )
+
+  it("validando campo enviou", () => {
+    cy.get('input[type="file"]')
+      .selectFile(
+        [
+          'cypress/fixtures/7K_de_Analise_das_Disciplinas_-_ADS.xlsx',
+          'cypress/fixtures/Teste Alunos.xlsx'
+        ],
+        {force: true})
+    cy.get('button[type="submit"]').click();
+  } )
+})
